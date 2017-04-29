3914b59500a6c6501838e31245f0120724d04164
Alexis Suarez <alexissuarez@Alexiss-MacBook-Pro.local>
2017-04-29 14:08:56 -0500

    Upload to Heroku with server.js created and fixed

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
 
+// Create our app::
+var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function(request, response, next){
+	//This header doesnt exists locally, is only for Heroku::
+	if (request.headers['x-forwarded-proto'] === 'https'){
+		response.redirect("http://" + request.hostname + request.url);
+	}else{
+		next();
+	}
+});
+
 app.use(express.logger('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
+//app.listen(process.env.PORT || 5000);
