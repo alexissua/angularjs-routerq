de05f5bf1fcafa218c2c950aaf6c7c604345a393
Alexis Suarez <alexissuarez@Alexiss-MacBook-Pro.local>
2017-04-30 12:10:43 -0500

    Grunt compile again..

--- server.js
+++ server.js
@@ -6,16 +6,8 @@ var app = express();
 var app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(function(request, response, next){
-	//This header doesnt exists locally, is only for Heroku::
-	if (request.headers['x-forwarded-proto'] === 'https'){
-		response.redirect("http://" + request.hostname + request.url);
-	}else{
-		next();
-	}
-});
 
-//app.use(express.logger('dev'));
+app.use(express.logger('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 
 app.listen(PORT, function(){
