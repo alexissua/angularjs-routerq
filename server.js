9292424176888c8418f1bf396fb872ea28c630e3
Alexis Suarez <alexissuarez@Alexiss-MacBook-Pro.local>
2017-04-30 12:19:03 -0500

    Commit with morgan added to server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var gzippo = require('gzippo');
 var express = require('express');
-var app = express();
+// We have to install morgan. Some of the middleware of express you have to install it separate::
+var morgan = require('morgan');
 
 // Create our app::
 var app = express();
 const PORT = process.env.PORT || 5000;
 
-
-app.use(express.logger('dev'));
+app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 
 app.listen(PORT, function(){
